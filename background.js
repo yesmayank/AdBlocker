43e8d22d142ed9da1c7b09cca578b1d36159de79
mayank mani tripathi <45062501+yesmayank@users.noreply.github.com>
2020-10-03 13:01:02 +0530

    added instructions to edit

--- background.js
+++ background.js
@@ -3,3 +3,6 @@ chrome.webRequest.onBeforeRequest.addListener(
 	{ urls: ["*://*.code.whitehatjr.com/*","*://*.zedo.com/*", "*://*.google.com/pagead/*", "*://*.google.com/aclk*","*://*.google.co.in/pagead/*", "*://*.youtube.com/pagead/*","*://*.youtube.com/api/stats/ads*", "*://*.codr.toppr.com/*", "*://*.wizklub.com/*"]},
 	["blocking"]
 )
+//append the list in line 3 to block more sites
+//eg:-
+// "*://*.xyz.com/*"
